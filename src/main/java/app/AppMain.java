376570a78a4be3b19f8242fbec3e7037f1369eb8
Denzel-Britton <denzel.britton573@gmail.com>
2022-08-22 16:19:27 -0500

    first commit

---
+++ src/main/java/app/AppMain.java
@@ -0,0 +1,17 @@
+package App;
+
+ import org.apache.commons.lang3.StringUtils;
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        Input input = new Input();
+        System.out.println("please enter something: ");
+        String user = input.getString();
+        System.out.println("You entered:  "  + user);
+        System.out.println("User input is numeric:  " + StringUtils.isNumeric(user));
+        System.out.println("Users input reverse:  "   + StringUtils.reverse(user));
+        System.out.println( "User input flipped:  "+ StringUtils.swapCase(user));
+    }
+}
